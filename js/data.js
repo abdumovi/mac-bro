ab6bdad9a4a0214ee8c59cb5e68ea1a4ba499031
abdurahmon <abdumovi@gmail.com>
2022-01-13 14:07:00 +0500

    ad

---
+++ js/data.js
@@ -0,0 +1,35 @@
+const imgs = [
+  [
+    "img/gold1.webp",
+    "img/gold2.webp",
+    "img/gold3.webp",
+    "img/gold4.webp",
+    "img/gold5.webp",
+
+  ],
+  [
+    "img/silver1.webp",
+    "img/silver2.webp",
+    "img/silver3.webp",
+    "img/silver4.webp",
+    "img/silver5.webp"
+  ],
+  [
+    "img/space1.webp",
+    "img/space2.webp",
+    "img/space3.webp",
+    "img/space4.webp",
+    "img/space5.webp",
+  ],
+]
+
+let prises = {
+  ram8:5000000,
+  ram16:7000000,
+  mem256:5000000,
+  mem512:7000000,
+  mem1:8000000,
+}
+
+  
+ 
---
+++ js/script.js
@@ -0,0 +1,54 @@
+const list = document.querySelector('.mac__list');
+const elbtns = document.querySelectorAll('.mac-btn');
+const elradio = document.querySelectorAll('.radios');
+const elColor = document.querySelectorAll('.radio__color');
+const elspan = document.querySelector('.sum');
+function corent(){
+  let resulPrice = 0;
+  for(let i = 0; i < elradio.length; i++){
+    if(elradio[i].checked){
+      for(let j in prises){
+        console.log(elradio[i].checked);
+        if(elradio[i].id == j){
+          resulPrice += prises[j];
+        }
+      }
+    }
+  }
+  return resulPrice;
+}
+corent()
+elradio.forEach((i)=>{
+  i.addEventListener('change',()=>{
+    elspan.textContent = corent();
+  });
+});
+let resul = 0;
+elbtns.forEach((i)=>{
+  i.addEventListener('click',(item)=>{
+    let initem = item.target.parentElement
+    resul = 700 * initem.value;
+    list.style.left = `-${resul - 700}px`;
+    for(let i of elbtns){
+      i.classList.remove('mac-btn-active');
+    }
+    if(initem.classList.contains('mac-btn')){
+      initem.classList.add('mac-btn-active');
+    }
+  });
+});
+elColor.forEach((i)=>{
+  i.addEventListener('change',(item)=>{
+    renderImg(item.target.value)
+  });
+});
+function renderImg(val){
+  list.innerHTML="";
+  for(let i = 0; i < imgs[val].length; i++){
+    let li = document.createElement('li');
+    li.className = 'mac__item';
+    li.innerHTML = `<img id="1" src="${imgs[val][i]}" alt="MacBook" class="current-img">`
+    list.append(li);
+    elbtns[i].innerHTML = `<img src="${imgs[val][i]}" alt="MacBook">`
+  }
+}
